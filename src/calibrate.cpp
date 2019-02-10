e8c097cb57c98b8a5d76ce5bd9f31f69457be9eb
shinaushin <shinaushin@gmail.com>
2019-02-10 14:30:00 -0500

    Detect circle correctly

--- src/calibrate.cpp
+++ src/calibrate.cpp
@@ -1,13 +1,28 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
+
+using namespace std;
 
 void callback(const sensor_msgs::ImageConstPtr& msg) {
     try {
         ROS_INFO("Image received");
-        cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
-        cv::waitKey(30);
+        cv::Mat image = cv_bridge::toCvShare(msg, "bgr8")->image;
+        vector<cv::Point2f> centers;
+        bool found = cv::findCirclesGrid(image, cv::Size(4,11), centers, cv::CALIB_CB_ASYMMETRIC_GRID);
+        ROS_INFO_STREAM(found);
+        // cv::imshow("view", image);
+        // cv::waitKey(30);
+        if (found) {
+            // cv::cornerSubPix(image, centers, cv::Size(11,11), cv::Size(-1,-1), cv::TermCriteria( cv::TermCriteria::EPS+cv::TermCriteria::COUNT, 30, 0.1 ));
+            cv::drawChessboardCorners(image, cv::Size(4,11), cv::Mat(centers), found);
+            cv::imshow("view", image);
+            cv::waitKey(30);
+        }
     }
     catch (cv_bridge::Exception& e) {
         ROS_ERROR("Could not convert");
--- src/send_images.cpp
+++ src/send_images.cpp
@@ -18,11 +18,13 @@ int main(int argc, char** argv) {
     int im_num = 56;
     cv::Mat image;
     sensor_msgs::ImagePtr msg;
+    float resize_factor = 0.16; // resize image to 640 x 480
     while(nh.ok() && i < 20) {
         string num = to_string(im_num+i);
         string path = ros::package::getPath("camera_calibration") + "/data/IMG_17"+num+".JPG";
         ROS_INFO_STREAM(path);
         image = cv::imread(path, CV_LOAD_IMAGE_COLOR);
+        cv::resize(image, image, cv::Size(), resize_factor, resize_factor);
         // cv::imshow("Display", image);
         // cv::waitKey(0);
         msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
