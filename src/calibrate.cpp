2861598b511f574e8866eee6675fbd4367683b1a
shinaushin <shinaushin@gmail.com>
2019-02-10 16:41:27 -0500

    wrote basic version of service containing listener/subscriber

--- src/calibrate.cpp
+++ src/calibrate.cpp
@@ -5,39 +5,73 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/calib3d.hpp>
+#include <camera_calibration/Calibrate.h>
 
 using namespace std;
 
-void callback(const sensor_msgs::ImageConstPtr& msg) {
-    try {
-        ROS_INFO("Image received");
-        cv::Mat image = cv_bridge::toCvShare(msg, "bgr8")->image;
-        vector<cv::Point2f> centers;
-        bool found = cv::findCirclesGrid(image, cv::Size(4,11), centers, cv::CALIB_CB_ASYMMETRIC_GRID);
-        ROS_INFO_STREAM(found);
-        // cv::imshow("view", image);
-        // cv::waitKey(30);
-        if (found) {
-            // cv::cornerSubPix(image, centers, cv::Size(11,11), cv::Size(-1,-1), cv::TermCriteria( cv::TermCriteria::EPS+cv::TermCriteria::COUNT, 30, 0.1 ));
-            cv::drawChessboardCorners(image, cv::Size(4,11), cv::Mat(centers), found);
-            cv::imshow("view", image);
-            cv::waitKey(30);
+class calibrate {
+
+    private:
+        vector<cv::Mat> images;
+        ros::NodeHandle nh;
+        ros::ServiceServer service;
+
+    public:
+
+        calibrate() {
+            service = nh.advertiseService("calibrate", &calibrate::calib, this);
         }
-    }
-    catch (cv_bridge::Exception& e) {
-        ROS_ERROR("Could not convert");
-    }
-}
+
+        void callback(const sensor_msgs::ImageConstPtr& msg) {
+            try {
+                ROS_INFO("Image received");
+        
+                images.push_back(cv_bridge::toCvShare(msg, "bgr8")->image);
+
+                if (images.size() == 20) {
+                    vector<vector<cv::Point3f>> centers_allImages;
+                    for (int i = 0; i < 20; i++) {
+                        vector<cv::Point2f> centers;
+                        cv::Mat image = images[i];
+                        bool found = cv::findCirclesGrid(image, cv::Size(4,11), centers, cv::CALIB_CB_ASYMMETRIC_GRID);
+                        // cv::imshow("view", image);
+                        // cv::waitKey(30);
+                        if(found) {
+                            // cv::cornerSubPix(image, centers, cv::Size(11,11), cv::Size(-1,-1), cv::TermCriteria( cv::TermCriteria::EPS+cv::TermCriteria::COUNT, 30, 0.1 ));
+                            cv::drawChessboardCorners(image, cv::Size(4,11), cv::Mat(centers), found);
+                            cv::imshow("view", image);
+                            cv::waitKey(30);
+                        }
+                    }
+            
+                    images.clear();
+                }
+            }
+            catch (cv_bridge::Exception& e) {
+                ROS_ERROR("Could not convert");
+            }
+        }  
+
+        bool calib(camera_calibration::Calibrate::Request &req, camera_calibration::Calibrate::Response &res) {
+            string mode = req.mode;
+            if (mode == "calibrate") {
+                image_transport::ImageTransport it(nh);
+                image_transport::Subscriber sub = it.subscribe("image_raw", 20, &calibrate::callback, this);
+            }
+    
+            return true;
+        }
+};
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "image_listener");
-    ros::NodeHandle nh;
+    
     cv::namedWindow("view");
     cv::startWindowThread();
-    image_transport::ImageTransport it(nh);
-    image_transport::Subscriber sub = it.subscribe("image_raw", 20, callback);
 
     ros::spin();
     cv::destroyWindow("view");
+
+    return 0;
 }
 
